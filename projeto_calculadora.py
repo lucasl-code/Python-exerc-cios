38ff43200f2a8e652301b8bc6a197a8dcaa3e4d0
lucaslima <lucaaslimadls@gmail.com>
2026-04-21 14:07:05 -0400

    modificando a estrutura do projeto

--- projeto_calculadora.py
+++ projeto_calculadora.py
@@ -1,1 +1,22 @@
-rfifg
+# simualdor de login
+# utilizando o loop for
+
+# definindo variáveis usuario e senha
+# atribuindo o valor correto
+usuario_correto = "gabrielzinho"
+senha_correta = 12345
+
+# máximo de tentativas é 5
+for _ in range(5):
+    usuario = input("Digite seu usuário: ")
+    senha = int(input("Digite sua senha: "))
+    if usuario == usuario_correto and senha == senha_correta:
+        print("Credenciais válidas")
+        break
+    else:
+        print("Credenciais inválidas, tente novamente")
+
+else:
+    print("Número máximo de tentativas excedidas")
+
+print("Foi um sucesso o projeto")
