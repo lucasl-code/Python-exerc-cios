f5e3dee89c7391a81300bba4c85c84bdfd1590c8
lucaslima <lucaaslimadls@gmail.com>
2026-04-21 14:48:03 -0400

    modifiquei o nome e a estrutura do arquivo

---
+++ loops-execícios/exercicio_pessoas.py
@@ -0,0 +1,7 @@
+# Faça um loop para imprimir a frase ("Olá, {nome da pessoa}") 
+# Para cada pessoa da lista abaixo
+
+pessoas = ["Pedro", "Gustavo", "Gabriel", "Mário"]
+
+for nome in pessoas:
+    print(f"Olá, {nome}")
